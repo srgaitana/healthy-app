afa78f8d80bc500b2ecb8ce502b4da0ad80888f3
srgaitana <srgaitana@unal.edu.co>
2024-11-12 21:22:41 -0500

    Updating api/user and lib db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,11 @@
 import mysql from 'mysql2';
 
+// Usar variables de entorno para la conexión a la base de datos
 const pool = mysql.createPool({
-  host: 'localhost',  
-  user: 'root', 
-  password: 'Laurita12*',
-  database: 'HealthyAppDB',
+  host: process.env.DB_HOST,  
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 pool.getConnection((err, connection) => {
